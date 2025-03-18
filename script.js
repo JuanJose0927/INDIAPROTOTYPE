0da9dd48bfdd8f96ee81791d03b9c42ba800233c
JuanJose0927 <juanjosearbelaez689@gmail.com>
2025-03-18 09:59:23 -0500

    finished

--- script.js
+++ script.js
@@ -38,6 +38,22 @@ loadCards();
 
 
 
+//Scroll de section
+
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible"); // Para que se reinicie
+    }
+  });
+}, { threshold: 0.2 });
+
+sections.forEach(section => observer.observe(section));
+
 
 
 
